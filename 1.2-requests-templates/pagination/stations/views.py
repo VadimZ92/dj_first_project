8b5ecabc3cef1bd581028e007ce40c87cc3528d9
VadimZ92 <hemulyaa@gmail.com>
2022-10-25 22:19:21 +0500

    update recipes and pagination

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
+import csv, os
+from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 def index(request):
-    return redirect(reverse('bus_stations'))
+    # return redirect(reverse('bus_stations'))
+    return render(request, 'index.html')
 
+bus_stations_list = []
+with open('C:\\Users\\hemul\\Desktop\\Нетология\\projects\\django\\first_project\\dj-homeworks\\1.2-requests-templates\\pagination\\data-398-2018-08-30.csv', newline='', encoding="utf-8") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        bus_stations_list.append(row)
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
+    page_number = int(request.GET.get("page", 1))
+    paginator = Paginator(bus_stations_list, 10)
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': bus_stations,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
+from django.urls import path, include
+from calculator.views import omlet, pasta, buter
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('omlet/', omlet),
+    path('pasta/', pasta),
+    path('buter/', buter),
 ]
